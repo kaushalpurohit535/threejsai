6108336a0514ea544805c4200d129f36305bd5fa
Kaushal Purohit <117456158+kaushalpurohit535@users.noreply.github.com>
2023-08-25 20:24:30 +0530

    Add files via upload

---
+++ client/src/store/index.js
@@ -0,0 +1,29 @@
+// import { proxy } from 'valtio';
+
+// const state = proxy({
+//     intro: true,
+//     color: '#EFBD48',
+//     isLogoTexture: true,
+//     isFullTexture: false,
+//     logoDecal: './threejs.png',
+//     fullDecal: './threejs.png'
+// });
+// export default state;
+
+
+
+
+
+
+import { proxy } from 'valtio';
+
+const state = proxy({
+  intro: true,
+  color: '#EFBD48',
+  isLogoTexture: true,
+  isFullTexture: false,
+  logoDecal: './threejs.png',
+  fullDecal: './threejs.png',
+});
+
+export default state;
